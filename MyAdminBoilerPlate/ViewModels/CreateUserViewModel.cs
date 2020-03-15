10f918ec6287a800a7b55a3774f6840be489abb9
cidospark <cidospark27@gmail.com>
2020-03-15 07:48:52 +0100

    added CreateUserViewModel

---
+++ MyAdminBoilerPlate/ViewModels/CreateUserViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using MyAdminBoilerPlate.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyAdminBoilerPlate.ViewModels
+{
+    public class CreateUserViewModel
+    {
+        public string LastName { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        public string Email { get; set; }
+        [Required]
+        public Gender Gender { get; set; }
+        public string Nationality { get; set; }
+        public int DOB { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public IFormFile Pix { get; set; }
+    }
+}
