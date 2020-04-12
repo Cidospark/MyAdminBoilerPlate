ad41eb38e858dbb71af3d0d94a2e326f6e2b75ef
cidospark <cidospark27@gmail.com>
2020-04-12 08:11:20 +0100

    Added user claims manager

--- MyAdminBoilerPlate/Controllers/AdministrationController.cs
+++ MyAdminBoilerPlate/Controllers/AdministrationController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -31,6 +32,72 @@ namespace MyAdminBoilerPlate.Controllers
             this.logger = logger;
         }
 
+        // manage user claims action method
+        [HttpGet]
+        public async Task<IActionResult> ManageUserClaims(string userId)
+        {
+            // get user by Id
+            var user = await userManager.FindByIdAsync(userId);
+            if(user == null)
+            {
+                ViewBag.ErrorMessage = $"User with id = {userId} cannot be found";
+                return View("NotFound");
+            }
+
+            var existingUserClaim = await userManager.GetClaimsAsync(user);
+            var model = new UserClaimsViewModel
+            {
+                UserId = userId
+            };
+            
+            foreach(Claim claim in ClaimsStore.AllClaims)
+            {
+                UserClaim userClaim = new UserClaim
+                {
+                    ClaimType = claim.Type
+                };
+
+                if(existingUserClaim.Any(c => c.Type == claim.Type))
+                {
+                    userClaim.IsSelected = true;
+                }
+
+                model.Claims.Add(userClaim);
+            }
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> ManageUserClaims(UserClaimsViewModel model)
+        {
+            var user = await userManager.FindByIdAsync(model.UserId);
+            if(user == null)
+            {
+                ViewBag.ErrorMessage = $"User with id = {model.UserId} cannot be found";
+                return View("NotFound");
+            }
+
+            // if we have found the user
+            // retrieve all the user claims and delete those claims
+            var claims = await userManager.GetClaimsAsync(user);
+            var result = await userManager.RemoveClaimsAsync(user, claims);
+            // if there is any problem removing the claims then
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError("", "Cannot remove user existing claims");
+                return View(model);
+            }
+
+            // retrieve all selected claims
+            result = await userManager.AddClaimsAsync(user, model.Claims.Where(c => c.IsSelected)
+                .Select(c => new Claim(c.ClaimType, c.ClaimType)));
+
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError("", "Cannot add selected claims to user");
+                return View(model);
+            }
+            return RedirectToAction("Edit", "Account", new { id = model.UserId });
+        }
 
         [HttpGet]
         public async Task<IActionResult> ManageUserRoles(string userId)
---
+++ MyAdminBoilerPlate/Models/ClaimsStore.cs
@@ -0,0 +1,18 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace MyAdminBoilerPlate.Models
+{
+    public static class ClaimsStore
+    {
+        public static List<Claim> AllClaims = new List<Claim>()
+        {
+            new Claim("Create Role", "Create Role"),
+            new Claim("Edit Role", "Delete Role"),
+            new Claim("Delete Role", "Delete Role")
+        };
+    }
+}
